a5238304adc826822f7002fc4b022cd35591b791
sreejithNeoito <sreejithcp@neoito.com>
2025-07-10 14:21:51 +0530

    Add custom field for homepage customization

---
+++ wp-content/themes/happilee/inc/home_page_cf.php
@@ -0,0 +1,77 @@
+<?php
+
+function cmb2_home_metaboxes()
+{
+    $prefix = 'home_page_';
+
+    $cmb = new_cmb2_box( array(
+        'id'            => $prefix . 'metabox',
+        'title'         => __( 'Statistics of Use Case Effectiveness', 'happilee' ),
+        'object_types'  => array( 'page' ),
+        'context'       => 'normal',
+        'priority'      => 'high',
+        'show_names'    => true,
+        'show_on_cb'    => 'show_on_front_page',
+    ) );
+
+    $useCase_group_id = $cmb->add_field( array(
+		'id'             => 'use_case_statics',
+		'type'           => 'group',
+		'repeatable'     =>  true,
+        'repeatable_max' => 4,
+		'options'        => array(
+			'group_title'   => 'Statistic {#}',
+			'add_button'    => 'Add Another Field',
+			'remove_button' => 'Remove Field',
+			'closed'        => true,
+			'sortable'      => true,
+		),
+	) );
+	$cmb->add_group_field( $useCase_group_id, array(
+		'name'      => __('Section Title', 'happilee'),
+		'desc'      => __('Enter the title of the section.','happilee'),
+		'id'        => 'case_id_1',
+		'type'      => 'wysiwyg',
+        'options'   => array(
+            'textarea_rows' => 5,
+            'media_buttons' => false,
+            'teeny' => true, // Simplified toolbar
+        ),
+	) );
+
+    $cmb->add_group_field( $useCase_group_id, array(
+		'name'               => __('SVG Icon', 'happilee'),
+		'desc'               => __('Paste your SVG markup here.', 'happilee' ),
+		'id'                 => 'case_id_3',
+		'type'               => 'textarea_code',
+        'options'            => array(
+            'disable_codemirror' => false,
+        ),
+	) );
+
+    $cmb->add_group_field( $useCase_group_id, array(
+		'name' => __('User Count', 'happilee'),
+		'desc' => __('Enter the number of customers (e.g., 1200+, 3K, 4M, etc.)', 'happilee' ),
+		'id'   => 'case_id_2',
+		'type' => 'text',
+	) );
+
+}
+add_action('cmb2_admin_init', 'cmb2_home_metaboxes');
+
+function show_on_front_page( $cmb ) {
+
+    $screen = get_current_screen();
+    
+    if ( $screen && $screen->id === 'page' ) {
+        $page_id = isset( $_GET['post'] ) ? intval( $_GET['post'] ) : 0;
+        
+        if ( $page_id ) {
+            // Check if this page is set as front page
+            $front_page_id = get_option( 'page_on_front' );
+            return $page_id == $front_page_id;
+        }
+    }
+    
+    return false;
+}
