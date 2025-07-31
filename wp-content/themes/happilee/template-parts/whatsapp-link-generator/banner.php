92459c5a31fd5a073f8bc8592ac2e1c21a6ecfa5
sreejithNeoito <sreejithcp@neoito.com>
2025-07-31 17:05:15 +0530

    Change whatsapp link generator banner image

--- wp-content/themes/happilee/template-parts/whatsapp-link-generator/banner.php
+++ wp-content/themes/happilee/template-parts/whatsapp-link-generator/banner.php
@@ -1,5 +1,5 @@
 <section class="container flex gap-10 py-10 mdd:flex-col mdd:py-0 mdd:px-5 mdd:gap-5 items-end">
-    <div class="flex w-1/2 p-8 gap-6 flex-col mdd:w-full order-2 md:order-1 mdd:p-4 mdd:gap-6">
+    <div class="flex w-1/2 lg:flex-1 p-8 gap-6 flex-col mdd:w-full order-2 md:order-1 mdd:p-4 mdd:gap-6">
         <div class="rounded-[10px] w-max text-20 leading-[22px] text-primary">WhatsApp Link <b>Generator</b></div>
         <h1 class="font-semibold text-40 leading-[44px] text-primary">One-click WhatsApp chat links for your social media pages</h1>
         <p class="text-16 leading-[24px]">Make it easy for your followers to reach you on WhatsApp with one-click chat links. Add them to your Instagram, Facebook, or any social profile.</p>
