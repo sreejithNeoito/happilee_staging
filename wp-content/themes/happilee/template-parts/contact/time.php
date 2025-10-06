fb03daf70a4a9d4f3acb1b70e9f94dee5149bf88
sreejithNeoito <sreejithcp@neoito.com>
2025-10-06 17:23:44 +0530

    refactor: improve layout and styles of Contact Us page

--- wp-content/themes/happilee/template-parts/contact/time.php
+++ wp-content/themes/happilee/template-parts/contact/time.php
@@ -1,8 +1,8 @@
 <section class="mdd:p-5">
     <div class="container  text-primary py-10">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-8 container bg-white rounded-[40px]">
+        <div class="grid grid-cols-1 md:grid-cols-3 bg-white rounded-[40px]">
 
-            <div class="w-full space-y-6">
+            <div class="w-full space-y-6 p-8">
                 <div>
                     <h1 class="text-24  font-normal font-main text-primary">
                         Contact Us <br>
@@ -14,12 +14,12 @@
                     <p class="text-16">Email:<br><b><span class="text-[17px]">sales@happilee.io</span></b></p>
                 </div>
             </div>
-            <div class="w-full space-y-6">
+            <div class="w-full space-y-6 p-8">
                 <h1 class="text-24  font-normal font-main text-primary">Reach us<br><b>Our Office</b></h1>
                 <p>NeoITO Technology Centre, Kuranganoor, Marappalam Road, Pattom, Thiruvananthapuram, Kerala, India-695004</p>
             
             </div>
-            <div class="w-full space-y-6">
+            <div class="w-full space-y-6 p-8">
                 <h1 class="text-24  font-normal font-main text-primary">
                     Our<br>
                     <b><span class="text-[#28B53E]">Business</span> Hours</b>
